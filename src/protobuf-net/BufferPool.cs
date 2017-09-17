12cb126231c53f70cde3c3d93059976822cbbdee
Rob Emery <mintsoft@users.noreply.github.com>
2017-09-17 20:27:02 +0100

    Merging bufferpool from master

--- src/protobuf-net/BufferPool.cs
+++ src/protobuf-net/BufferPool.cs
@@ -1,51 +1,78 @@
-﻿
-using System.Threading;
+﻿using System;
+
 namespace ProtoBuf
 {
+    public static class BufferPoolConfiguration
+    {
+        public static int PoolSize = 20;
+        public static int InitialBufferSize = 1024;
+    }
+
     internal sealed class BufferPool
     {
+        private BufferPool() { }
+
+        private static readonly CachedBuffer[] Pool = new CachedBuffer[BufferPoolConfiguration.PoolSize];
+
         internal static void Flush()
         {
-#if PLAT_NO_INTERLOCKED
-            lock(pool)
+            lock (Pool)
             {
-                for (int i = 0; i < pool.Length; i++) pool[i] = null;
+                for (var i = 0; i < Pool.Length; i++)
+                    Pool[i] = null;
             }
-#else
-            for (int i = 0; i < pool.Length; i++)
-            {
-                Interlocked.Exchange(ref pool[i], null); // and drop the old value on the floor
-            }
-#endif
         }
-        private BufferPool() { }
-        const int PoolSize = 20;
-        internal const int BufferLength = 1024;
-        private static readonly object[] pool = new object[PoolSize];
 
         internal static byte[] GetBuffer()
         {
-            object tmp;
-            #if PLAT_NO_INTERLOCKED
-            lock(pool)
+            return GetBuffer(BufferPoolConfiguration.InitialBufferSize);
+        }
+
+        internal static byte[] GetBuffer(int minSize)
+        {
+            byte[] cachedBuff = GetCachedBuffer(minSize);
+            return cachedBuff ?? new byte[minSize];
+        }
+
+        internal static byte[] GetCachedBuffer(int minSize)
+        {
+            lock (Pool)
             {
-                for (int i = 0; i < pool.Length; i++)
+                var bestIndex = -1;
+                byte[] bestMatch = null;
+                for (var i = 0; i < Pool.Length; i++)
                 {
-                    if((tmp = pool[i]) != null)
+                    var buffer = Pool[i];
+                    if (buffer == null || buffer.Size < minSize)
                     {
-                        pool[i] = null;
-                        return (byte[])tmp;
+                        continue;
+                    }
+                    if (bestMatch != null && bestMatch.Length < buffer.Size)
+                    {
+                        continue;
+                    }
+
+                    var tmp = buffer.Buffer;
+                    if (tmp == null)
+                    {
+                        Pool[i] = null;
+                    }
+                    else
+                    {
+                        bestMatch = tmp;
+                        bestIndex = i;
                     }
                 }
+
+                if (bestIndex >= 0)
+                {
+                    Pool[bestIndex] = null;
+                }
+
+                return bestMatch;
             }
-#else
-            for (int i = 0; i < pool.Length; i++)
-            {
-                if ((tmp = Interlocked.Exchange(ref pool[i], null)) != null) return (byte[])tmp;
-            }
-#endif
-            return new byte[BufferLength];
         }
+
         internal static void ResizeAndFlushLeft(ref byte[] buffer, int toFitAtLeastBytes, int copyFromIndex, int copyBytes)
         {
             Helpers.DebugAssert(buffer != null);
@@ -53,51 +80,82 @@ namespace ProtoBuf
             Helpers.DebugAssert(copyFromIndex >= 0);
             Helpers.DebugAssert(copyBytes >= 0);
 
-            // try doubling, else match
-            int newLength = buffer.Length * 2;
+            var newLength = buffer.Length * 2;
             if (newLength < toFitAtLeastBytes) newLength = toFitAtLeastBytes;
 
-            byte[] newBuffer = new byte[newLength];
+            if (copyBytes == 0)
+            {
+                ReleaseBufferToPool(ref buffer);
+            }
+
+            var newBuffer = GetCachedBuffer(toFitAtLeastBytes) ?? new byte[newLength];
+
             if (copyBytes > 0)
             {
                 Helpers.BlockCopy(buffer, copyFromIndex, newBuffer, 0, copyBytes);
+                ReleaseBufferToPool(ref buffer);
             }
-            if (buffer.Length == BufferPool.BufferLength)
-            {
-                BufferPool.ReleaseBufferToPool(ref buffer);
-            }
+
             buffer = newBuffer;
         }
+
         internal static void ReleaseBufferToPool(ref byte[] buffer)
         {
-            if (buffer == null) return;
-            if (buffer.Length == BufferLength)
+            if (buffer == null)
+                return;
+
+            lock (Pool)
             {
-#if PLAT_NO_INTERLOCKED
-                lock (pool)
+                var minIndex = 0;
+                var minSize = int.MaxValue;
+                for (var i = 0; i < Pool.Length; i++)
                 {
-                    for (int i = 0; i < pool.Length; i++)
+                    var tmp = Pool[i];
+                    if (tmp == null || !tmp.IsAlive)
                     {
-                        if(pool[i] == null)
-                        {
-                            pool[i] = buffer;
-                            break;
-                        }
+                        minIndex = 0;
+                        break;
+                    }
+                    if (tmp.Size < minSize)
+                    {
+                        minIndex = i;
+                        minSize = tmp.Size;
                     }
                 }
-#else
-                for (int i = 0; i < pool.Length; i++)
-                {
-                    if (Interlocked.CompareExchange(ref pool[i], buffer, null) == null)
-                    {
-                        break; // found a null; swapped it in
-                    }
-                }
-#endif
+
+                Pool[minIndex] = new CachedBuffer(buffer);
             }
-            // if no space, just drop it on the floor
+
             buffer = null;
         }
 
+        public static int[] GetBufferPoolSizes()
+        {
+            var bufferSizes = new int[BufferPoolConfiguration.PoolSize];
+            lock(Pool)
+            {
+                for (var i = 0; i < Pool.Length; i++)
+                {
+                    bufferSizes[i] = Pool[i] == null ? -1 : Pool[i].Size;
+                }
+            }
+            return bufferSizes;
+        }
+
+        private class CachedBuffer
+        {
+            private readonly WeakReference _reference;
+
+            public int Size { get; }
+
+            public bool IsAlive => _reference.IsAlive;
+            public byte[] Buffer => (byte[])_reference.Target;
+
+            public CachedBuffer(byte[] buffer)
+            {
+                Size = buffer.Length;
+                _reference = new WeakReference(buffer);
+            }
+        }
     }
 }
